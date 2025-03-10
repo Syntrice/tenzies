44f81fc0835fbf78467cbcdfd28313f7e02a91fe
Syntrice <syntrice@protonmail.com>
2025-03-10 21:16:44 +0000

    feat: Add a button to main to generate random dice
    Add a button component to main to update the dice numbers to a new set
    of random digits between 1-6.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,3 +1,4 @@
+import Button from "../common/Button"
 import Die from "../die/Die"
 import React from "react"
 
@@ -7,13 +8,16 @@ export default function Main() {
     // This function generates an array of length 10 with random numbers between 1 and 6
     return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1)
   }
-  
+
   const [numbers, setNumbers] = React.useState<number[]>(generateRandomNumbers())
 
   return (
-    <main className="bg-theme-light m-10 flex h-screen max-h-[200px] w-screen max-w-[400px] flex-col items-center justify-center rounded-xl shadow-2xl">
+    <main className="bg-theme-light m-10 flex h-screen max-h-[400px] w-screen max-w-[400px] flex-col items-center justify-center gap-20 rounded-xl shadow-2xl">
       <div className="grid h-fit w-fit grid-cols-5 grid-rows-2 place-items-center gap-5">
-        { numbers.map(n => <Die key={n} number={n}/>) }
+        { numbers.map(n => <Die number={n}/>) }
+      </div>
+      <div>
+        <Button onClick={() => setNumbers(generateRandomNumbers())}>Roll</Button>
       </div>
     </main>
   )
