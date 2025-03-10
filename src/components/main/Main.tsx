5e20454b2e7e3324b739a08e26da11aadc227ad2
Syntrice <syntrice@protonmail.com>
2025-03-10 20:51:45 +0000

    feat: Implement generation of random Die numbers
    Add a function to main to generate an array with 10 random numbers
    between 1 and 6. This is done by using the Array.from method, and
    passing an object which a length property to specify the size. This
    essentially mocks an arraylike / iterable object length property.

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,19 +1,19 @@
 import Die from "../die/Die"
+import React from "react"
 
 export default function Main() {
+
+  function generateRandomNumbers(): number[] {
+    // This function generates an array of length 10 with random numbers between 1 and 6
+    return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1)
+  }
+  
+  const [numbers, setNumbers] = React.useState<number[]>(generateRandomNumbers())
+
   return (
     <main className="bg-theme-light m-10 flex h-screen max-h-[200px] w-screen max-w-[400px] flex-col items-center justify-center rounded-xl shadow-2xl">
       <div className="grid h-fit w-fit grid-cols-5 grid-rows-2 place-items-center gap-5">
-        <Die number={1} />
-        <Die number={2} />
-        <Die number={3} />
-        <Die number={4} />
-        <Die number={5} />
-        <Die number={6} />
-        <Die number={5} />
-        <Die number={4} />
-        <Die number={3} />
-        <Die number={2} />
+        { numbers.map(n => <Die key={n} number={n}/>) }
       </div>
     </main>
   )
