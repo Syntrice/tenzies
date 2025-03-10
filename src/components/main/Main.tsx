d644855ecc81a9c7ec6b94fb74181368aa10a121
Syntrice <syntrice@protonmail.com>
2025-03-10 21:27:55 +0000

    feat: Add more pronounced shadow to Main

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -12,7 +12,7 @@ export default function Main() {
   const [numbers, setNumbers] = React.useState<number[]>(generateRandomNumbers())
 
   return (
-    <main className="bg-theme-light m-10 flex h-screen max-h-90 w-screen max-w-90 flex-col items-center p-10 justify-center gap-8 rounded-xl shadow-2xl">
+    <main className="bg-theme-light m-10 flex h-screen max-h-90 w-screen max-w-90 flex-col items-center p-10 justify-center gap-8 rounded-xl shadow-[10px_10px_3px_0px_rgba(255,255,255,0.25)]">
       <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="grid h-fit w-fit grid-cols-5 grid-rows-2 place-items-center gap-5">
         { numbers.map(n => <Die number={n}/>) }
