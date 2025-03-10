07435da5d5edeab49569f5c6465a5d7f4380eada
syntrice <syntrice@protonmail.com>
2025-03-10 13:48:34 +0000

    feat: Setup basic styling for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <main><p>Hello World!</p></main>;
+  return <main className="w-screen h-screen p-5 max-h-[400px] max-w-[400px] m-10 rounded-xl bg-theme-light">
+    
+  </main>
 }
